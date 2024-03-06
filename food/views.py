83be54f32d3c4d1f5a9cdccac89bc7263c9d70e6
Saturnfive <bevane@live.com>
2024-03-06 14:34:47 +0700

    feat: add form to log food to food/index

--- food/views.py
+++ food/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Food
 
 # Create your views here.
 def index(request):
+    food = Food.objects.all()
+    if request.method == "POST":
+        food_name = request.POST["food_name"]
+        return HttpResponseRedirect(reverse("index"))
     return render(request, "food/index.html")
