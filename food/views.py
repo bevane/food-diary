b73161c861ab8f46a4602ad63cfe661daa670bad
Saturnfive <bevane@live.com>
2024-03-15 14:14:27 +0700

    feat: insert any new foods in form to Food table

--- food/views.py
+++ food/views.py
@@ -5,8 +5,11 @@ from .models import Food
 
 # Create your views here.
 def index(request):
-    food = Food.objects.all()
+    registered_foods = Food.objects.all()
     if request.method == "POST":
         food_name = request.POST["food_name"]
+        new_food = Food(name=f"{food_name}")
+        if not new_food.name.lower() in [food.name.lower() for food in registered_foods]:
+            new_food.save()
         return HttpResponseRedirect(reverse("index"))
     return render(request, "food/index.html")
