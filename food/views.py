e3b76eb106cd45342c25e1aed3004fd2a6aa3fb5
Saturnfive <bevane@live.com>
2024-03-17 14:03:33 +0700

    feat: show current utc time as default datetime in Food form

--- food/views.py
+++ food/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import Food
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
@@ -12,6 +13,10 @@ def index(request):
         if not new_food.name.lower() in [food.name.lower() for food in registered_foods]:
             new_food.save()
         return HttpResponseRedirect(reverse("index"))
+    
+    current_datetime = timezone.now()
+    current_datetime = current_datetime.strftime("%Y-%m-%dT%H:%M")
     return render(request, "food/index.html", {
-        "registered_foods": registered_foods
+        "registered_foods": registered_foods,
+        "current_datetime": current_datetime
     })
