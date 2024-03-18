a269a4a7249892f11b34b3c95ab2d9e2b5fe494b
Saturnfive <bevane@live.com>
2024-03-18 13:24:35 +0700

    feat: insert data on food form to food logs

--- food/views.py
+++ food/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from .models import Food
+from .models import Food, FoodLog
 from django.utils import timezone
 
 # Create your views here.
@@ -10,9 +10,16 @@ def index(request):
     print(registered_foods)
     if request.method == "POST":
         food_name = request.POST["food_name"]
+        log_datetime = request.POST["log-datetime"]
         new_food = Food(name=f"{food_name}")
+        new_log = FoodLog(
+            datetime=log_datetime,
+            food=new_food,
+            user = request.user,
+        )
         if not new_food.name.lower() in [food.name.lower() for food in registered_foods]:
             new_food.save()
+        new_log.save()
         return HttpResponseRedirect(reverse("food:index"))
     
     current_datetime = timezone.now()
