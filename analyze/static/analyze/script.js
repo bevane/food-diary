7bfae6edde00e29e25f26f355569e65a63ff3855
Saturnfive <bevane@live.com>
2024-03-29 16:50:33 +0700

    feat: add food chart to analyze app

--- analyze/static/analyze/script.js
+++ analyze/static/analyze/script.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const chartData = JSON.parse(document.getElementById('chart-data').textContent);
+    const symData = JSON.parse(document.getElementById('sym-data').textContent);
+    const foodData = JSON.parse(document.getElementById('food-data').textContent);
     const ctxSymptoms = document.getElementById('symptomsChart');
     const ctxFoods = document.getElementById('foodsChart')
+
+    // prepare dataset for the stacked bar chart for Food History
+    var foodDataset = []
+    for (let [key, value] of Object.entries(foodData[1])) {
+      foodDataset.push(
+        {
+          label: key,
+          data: value
+        })
+    }
 
     // instantiation and configuration of charts displayed on analyze app, using ChartJs
     new Chart(ctxSymptoms, {
@@ -9,9 +20,9 @@ document.addEventListener('DOMContentLoaded', function() {
       data: {
         datasets: [{
             label: 'All Symptoms',
-            data: chartData[1],
+            data: symData[1],
         }],
-        labels: chartData[0]
+        labels: symData[0]
       },
       options: {
         plugins: {
@@ -40,4 +51,40 @@ document.addEventListener('DOMContentLoaded', function() {
         }
       }
       })
+
+      new Chart(ctxFoods, {
+        type: 'bar',
+        data: {
+          labels: foodData[0],
+          datasets: foodDataset,
+        },
+        options: {
+          plugins: {
+            title: {
+              display: true,
+              text: 'Food history over time'
+            }
+          },
+          scales: {
+            y: {
+              stacked: true,
+              title: {
+                display: true,
+                text: '# of Food'
+              },
+              suggestedMin: 0,
+              ticks: {
+                stepSize: 1
+              }
+            },
+            x: {
+              stacked: true,
+              title: {
+                display: true,
+                text: 'Date'
+              }
+            }
+          }
+        }
+        })
 });
--- analyze/views.py
+++ analyze/views.py
@@ -1,20 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-from django.urls import reverse
 from django.db.models import Count
 from symptoms.models import SymptomsLog
-from django.core import serializers
+from food.models import Food,FoodLog
+import pandas as pd
 
 # Create your views here.
 def index(request):
     symptoms_history = SymptomsLog.objects.filter(user=request.user)
-    count_per_date = symptoms_history.values("datetime__date").annotate(Count("id"))
-    count_per_date = list(count_per_date)
+    sym_count_per_date = symptoms_history.values("datetime__date").annotate(Count("id"))
+    sym_count_per_date = list(sym_count_per_date)
+    sym_data_x_values = [i["datetime__date"] for i in sym_count_per_date]
+    sym_data_y_values = [i["id__count"] for i in sym_count_per_date]
+    sym_data = [sym_data_x_values, sym_data_y_values]
 
-    data_dates = [i["datetime__date"] for i in count_per_date]
-    data_id_counts = [i["id__count"] for i in count_per_date]
-    data = [data_dates, data_id_counts]
+    foods = Food.objects.all()
+    food_history = FoodLog.objects.filter(user=request.user)
+
+    # The food history data needs to be provided as 2 arrays
+    # the first array is list of dates
+    # the second array contains list of counts for each Food for the list of dates
+    # pivot table function on pandas is used to generate the data according to the above
+    # required format since sqlite does not have pivot queries
+    foods_df = pd.DataFrame.from_records(foods.values())
+    food_history_df = pd.DataFrame.from_records(food_history.values())
+    food_data_df = (food_history_df.merge(foods_df, left_on="food_id", right_on="id", how="outer")
+                                   .assign(date=lambda x: x["datetime"].dt.date)
+                                   # assign a count of 1 for each row of food log
+                                   # so that it can be summed later in pivot_table
+                                   .assign(count=1)
+                                   .pivot_table(index="date", columns="name",
+                                                values="count", fill_value=0, aggfunc="sum")
+    )
+    food_data_x_values = food_data_df.index.values.tolist()
+    food_data_y_values = food_data_df.to_dict(orient="list")
+    food_data = [food_data_x_values, food_data_y_values]
 
     return render(request, "analyze/index.html", {
-        "data":data
+        "sym_data":sym_data,
+        "food_data": food_data
     })
