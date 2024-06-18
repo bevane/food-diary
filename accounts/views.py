58ad8a8a63a2d316297a491197ba1ac490f9053b
Saturnfive <bevane@live.com>
2024-06-18 12:58:20 +0700

    feat: add sign up page

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignupForm
 
-# Create your views here.
 def signup(request):
-    pass
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/")
+    else:
+        form = SignupForm()
+    return render(request, "registration/signup.html", {"form": form})
+
